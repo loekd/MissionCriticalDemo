8b6eeec52acae72bec88938dfc9ebedcd950defd
LoekD <loekd@hotmail.com>
2022-11-09 10:26:21 +0100

    feat(transaction): add transaction example, use mongodb with transaction support

--- MissionCriticalDemo/MissionCriticalDemo.DispatchApi/Controllers/DispatchController.cs
+++ MissionCriticalDemo/MissionCriticalDemo.DispatchApi/Controllers/DispatchController.cs
@@ -7,6 +7,7 @@ using Microsoft.Identity.Web.Resource;
 using MissionCriticalDemo.DispatchApi.Hubs;
 using MissionCriticalDemo.DispatchApi.Services;
 using MissionCriticalDemo.Messages;
+using System.Text.Json;
 
 namespace MissionCriticalDemo.DispatchApi.Controllers
 {
@@ -52,14 +53,20 @@ namespace MissionCriticalDemo.DispatchApi.Controllers
                 //validate
                 int delta = request.Direction == Shared.Enums.FlowDirection.Inject ? request.AmountInGWh : 0 - request.AmountInGWh;
                 var currentTotal = await _gasStorage.GetGasInStore(_userId.GetValueOrDefault());
-                //validate
                 if (currentTotal + delta < 0)
                 {
                     throw new InvalidOperationException($"Unable to withdraw more gas ({delta}) than customer {_userId} currently has in store ({currentTotal}).");
                 }
 
                 var message = _mappers.ToMessage(request, _userId.GetValueOrDefault());
-                await _daprClient.SaveStateAsync(_stateStoreName, request.RequestId.ToGuidString(), message);
+                var requests = new List<StateTransactionRequest>()
+                {
+                    new StateTransactionRequest(request.RequestId.ToGuidString(), JsonSerializer.SerializeToUtf8Bytes(message), StateOperationType.Upsert),
+                    //TODO: add additional state changes in same transaction
+                };
+                
+                //save data in transaction
+                await _daprClient.ExecuteStateTransactionAsync(_stateStoreName, requests, cancellationToken : CancellationToken.None);
             }
             catch (Exception ex)
             {
