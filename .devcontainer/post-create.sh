033476d1e8efa7540736fbcbd2fc0c440b75103d
Loek Duys <loekd@hotmail.com>
2024-04-22 12:08:54 +0000

    fix(qa): prefetch and build images

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,3 +13,14 @@ done
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 dapr uninstall # clean if needed
 dapr init -k
+
+
+## Build the containers
+cd /workspaces/MissionCriticalDemo/MissionCriticalDemo
+docker-compose -f docker-compose.yml -f docker-compose.override.yml build
+
+docker pull mongo:7.0
+docker pull bitnami/mongodb:6.0.2
+docker pull mongo-express
+docker pull redis
+docker pull redis/redisinsight:latest
