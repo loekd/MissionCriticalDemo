972dada029893b9a9fd635370aacdc23ef15e28e
Loek Duys <loekd@hotmail.com>
2024-04-23 06:34:13 +0000

    fix(devcontainer): fix port issues and obsolete stuff

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -31,13 +31,20 @@ docker pull jaegertracing/all-in-one:1.6
 #docker pull openzipkin/zipkin:2.23.4
 
 
-# turn dispatch port to public (to allow CORS)
+## turn dispatch port to public (to allow CORS)
 gh codespace ports visibility 5133:public -c $CODESPACE_NAME
 
-
-echo "Editing the file '/workspaces/MissionCriticalDemo/MissionCriticalDemo/MissionCriticalDemo.FrontEnd/wwwroot/appsettings.json' and put https://$CODESPACE_NAME-5133.app.github.dev as value for DispatchApi:Endpoint"
+## configure the dispatch api endpoint
 export URL="https://$CODESPACE_NAME-5133.app.github.dev"
+echo "Editing the file '/workspaces/MissionCriticalDemo/MissionCriticalDemo/MissionCriticalDemo.FrontEnd/wwwroot/appsettings.json' and put $URL as value for DispatchApi:Endpoint"
 jq --arg newEndpoint "$URL" '.DispatchApi.Endpoint = $newEndpoint' /workspaces/MissionCriticalDemo/MissionCriticalDemo/MissionCriticalDemo.FrontEnd/wwwroot/appsettings.json > temp.json && mv temp.json /workspaces/MissionCriticalDemo/MissionCriticalDemo/MissionCriticalDemo.FrontEnd/wwwroot/appsettings.json
-echo "Make sure to add the proper redirect uri to Azure AD as well!"
 
-echo "Run `docker-compose -f docker-compose.yml -f docker-compose.override.yml up` to run the containers"
+## notify user about the redirect url
+export URL="https://$CODESPACE_NAME-8089.app.github.dev/authentication/login-callback"
+echo "Make sure to add this redirect uri \"$URL\" to Azure AD as well!"
+
+## hint to run the containers
+echo "Run \"docker-compose -f docker-compose.yml -f docker-compose.override.yml up\" to run the containers"
+
+## mark repo as safe
+git config --global --add safe.directory /workspaces/MissionCriticalDemo
